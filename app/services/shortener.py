580d9645f2cf7a1dbdcea18658716b2e803614f0
Nadson Costa <146302133+nadson-costa@users.noreply.github.com>
2025-09-21 00:34:10 -0300

    Feat: Adicionada a lógica dos parametros de UTM

--- app/services/shortener.py
+++ app/services/shortener.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 from sqlalchemy.orm import Session
 from app.schemas import url_schema
 from app.models import url as url_model
@@ -17,7 +18,31 @@ def to_base62(number: int) -> str:
     return "".join(reversed(result))
     
 def create_short_url(db: Session, url_data: url_schema.URLCreate) -> url_model.URL:
-    db_url = url_model.URL(original_url=str(url_data.original_url))
+    utm_params = {
+        "utm_source": url_data.utm_source,
+        "utm_medium": url_data.utm_medium,
+        "utm_campaign": url_data.utm_campaign,
+        "utm_term": url_data.utm_term,
+        "utm_content": url_data.utm_content
+    }
+    
+    provided_utms = {k: v for k, v in utm_params.items() if v is not None}
+
+    final_original_url = str(url_data.original_url)
+
+    if provided_utms:
+        parsed_url = urlparse(final_original_url)
+        existing_params = parse_qs(parsed_url.query)
+        existing_params.update(provided_utms)
+        new_query_string = urlencode(existing_params, doseq=True)
+
+        final_original_url = urlunparse(
+            (parsed_url.scheme, parsed_url.netloc, parsed_url.path, parsed_url.params,
+             new_query_string, parsed_url.fragment)
+        )
+
+
+    db_url = url_model.URL(original_url=final_original_url)
     db.add(db_url)
     db.commit()
     db.refresh(db_url)
